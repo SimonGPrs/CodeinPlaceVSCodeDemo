7aa0eacff4645fa9d512dd849feca56ba46d224f
Simon Prs <simongrangepiras@gmail.com>
2024-06-04 15:08:57 +0200

    start basic python script
    just some tests in python functions

---
+++ my_program.py
@@ -0,0 +1,10 @@
+
+
+def even_mumber():
+    i = 0
+    while i < 50:
+        if i % 2 == 1:
+            print(i)
+        i = i +1
+
+even_mumber()
---
+++ second_program.py
@@ -0,0 +1,9 @@
+
+def convert_celcius_to_f(cel):
+    t_farenheit = cel * 1.8 + 32
+    print(t_farenheit )
+
+
+convert_celcius_to_f(23)
+
+
